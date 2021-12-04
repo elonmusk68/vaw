85d080f9237b3d747d37257845f6ade97ec2bf6b
elonmusk68 <79363476+elonmusk68@users.noreply.github.com>
2021-12-04 11:37:06 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function banan(){
+    background_iEMGEE=new Image(); 
+
+    background_iEMGEE.onload=uploadbackground;
+ 
+    background_iEMGEE .src= background_img;
+ 
+    rover_iEMGEE=new Image(); 
+
+    rover_iEMGEE.onload=uploadrover;
+    rover_iEMGEE .src= rover_img;
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
